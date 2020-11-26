e6a67d56202c98bb00c8d1716b3a97a2ebe5f337
TSKraak <tskraak@gmail.com>
2020-11-26 22:14:16 +0100

    Removed unused definitions

--- src/pages/MyLeaves/index.tsx
+++ src/pages/MyLeaves/index.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { selectUser } from "../../store/user/selectors";
 
 export default function MyLeavesPage() {
   const user = useSelector(selectUser);
-  const dispatch = useDispatch();
 
   if (!user.email) {
     return <Redirect to="/"></Redirect>;
   }
-
-  // useEffect(() => {
-  // dispatch
-  // }, [input])
 
   return (
     <div>
