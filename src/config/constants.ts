f565705ab7fb31119eeae26e22517d59717b6097
TSKraak <tskraak@gmail.com>
2020-12-02 12:30:42 +0100

    Changed API url without a / at the end

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import noPicture from "../images/noPicture.jpg";
 
 export const apiUrl =
-  process.env.API_URL || "https://leaves-backend.herokuapp.com/";
+  process.env.API_URL || "https://leaves-backend.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const noLeafPicture = noPicture;
